65d269c39d1794059edb23dd02d86e229f64800c
AnKoshinist <ankoushinist@protonmail.com>
2018-07-05 23:06:52 +0900

    Posting is almostly possible

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const puppeteer = require("puppeteer");
 
 
-const name = "てすや";
-const message = "栗田 is unko";
+const name = "てすや by AnKoushinist";
+const message = `
+****Wikiは、attorney***及び関連するもの達の情報をまとめるという理念のもと平成26年3月27日に、HoshuSokuhoにて誕生しました。
+Wikiというサイトに対して、このページを見て下さる方々は敷居が高いとお思いかもしれません。 実際、そのようなWikiもあるのでしょう。 しかし、当Wikiは、編集者と共に泣き共に笑うことができるサイトでありたいと思っております。 
+`;
 
 function asyncWait(ms) {
     return new Promise((resolve) => {
@@ -22,14 +25,17 @@ function asyncWait(ms) {
     //await page.goto('http://hosyusokuhou.jp/');
     await page.goto('http://hosyusokuhou.jp/archives/48819836.html');
     await page.evaluate((name, message) => {
-        $("input#name.form-name").val(name);
+        if (name) {
+            $("input#name.form-name").val(name);
+        }
         $("textarea[name='comment']").val(message);
         document.querySelector("input#name.form-name").scrollIntoView();
     }, name, message);
-    await asyncWait(20000);
-    await page.screenshot({
-        path: "formtest.png"
+    await asyncWait(5000);
+    await page.evaluate((name, message) => {
+        $("input[name='submit']").click();
     });
+    await asyncWait(20000);
 
     await browser.close();
 })();
