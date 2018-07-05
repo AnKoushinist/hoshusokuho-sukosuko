e0ce519a51a9a9761dfb7feb33b7b926bd1891e6
AnKoushinist <ankoushinist@protonmail.com>
2018-07-05 19:32:04 +0900

    Let's take a screenshot for my practice

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const puppeteer = require("puppeteer");
 
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false
+    });
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+    await page.goto('http://hosyusokuhou.jp/');
+    await page.setViewport({width: 1920, height: 1080});
     await page.screenshot({
-        path: 'example.png'
+        path: 'example.png',
+        fullPage: true
     });
 
     await browser.close();
