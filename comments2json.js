2e9f091aa7666e1dbde763ce6ffabc28353ebdf6
AnKoshinist <ankoushinist@protonmail.com>
2018-07-06 19:18:27 +0900

    Comments can be converted to JSON right now

--- comments2json.js
+++ comments2json.js
@@ -2,6 +2,8 @@ const cheerio = require("cheerio");
 const request = require("request");
 
 const extractor = /(?:http:\/\/)?(?:hosyusokuhou.jp\/archives\/)?([0-9]+)(?:\.html)?/;
+const dateExtractor = /[0-9]{4}年[0-9]{2}月[0-9]{2}日 [0-9]{2}:[0-9]{2}:[0-9]{2}/;
+const idExtractor = /ID:([a-zA-Z0-9]+)/;
 
 function requestGet(url, options) {
     return new Promise((resolve, reject) => {
@@ -22,4 +24,19 @@ async function convertNow(input) {
     const $ = cheerio.load(text);
 
     const commentsRoot = $("div#comments-list");
+    let data = [];
+    commentsRoot.children("div.com_header").each(function (index, element) {
+        const content = $(element);
+        const text = content.text();
+        const date = dateExtractor.exec(text)[0];
+        const postId = idExtractor.exec(text)[1];
+        const name = content.find("span.com_name").text();
+        const message = content.find("div.comtext").text();
+        data.push({
+            date, postId, name, message
+        });
+    });
+    return data;
 }
+
+module.exports = convertNow;
